b371f0c2c72d36cc2da86f223d16c1d587efb9d3
Parth2299 <126556736+Parth2299@users.noreply.github.com>
2023-03-19 00:17:47 +0530

    Update Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -1,6 +1,26 @@
 //Write a C code to find the number of distinct elements in a sorted array.
 
+#include <stdio.h>
+
 int main()
 {
-  return 0;
+    int arr[50], n, i, j = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the sorted array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] != arr[i + 1]) {
+            j++;
+        }
+    }
+
+    printf("The number of distinct elements in the array is: %d", count);
+
+    return 0;
 }
